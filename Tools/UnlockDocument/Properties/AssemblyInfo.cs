0808ff02d322f33c0062125412f141276273abd9
Corey Murtagh <Corey.Murtagh@mercuryit.com.au>
2015-08-15 11:21:54 +1000

    Trial of build auto-increment script.

--- Tools/UnlockDocument/Properties/AssemblyInfo.cs
+++ Tools/UnlockDocument/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -9,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("Unlock OMS Documents")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Mercury Technology Solutions Pty Ltd")]
-[assembly: AssemblyProduct("OMSUnlock")]
+[assembly: AssemblyProduct("OMSUnlock Test")]
 [assembly: AssemblyCopyright("Copyright © Mercury Technology Solutions Pty Ltd 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.24")]
+[assembly: AssemblyFileVersion("1.0.0.24")]
